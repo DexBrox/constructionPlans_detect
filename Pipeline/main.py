1f0b1112eb7140f694a2ed1bf6a69d412c84db67
DexBrox <jung.valentin98@gmail.com>
2024-05-03 18:20:20 +0200

    extending pipeline

--- Pipeline/main.py
+++ Pipeline/main.py
@@ -4,6 +4,10 @@ from detect_evaluation_functions import *
 
 import pytesseract
 from PIL import Image
+from tqdm import tqdm  # Import von tqdm
+
+import logging
+logging.getLogger('ultralytics').setLevel(logging.ERROR)
 
 dataset_path = '/workspace/datasets/Roewaplan/images/test'
 model_path = '/workspace/main_folder/models/best.pt'
@@ -22,26 +26,41 @@ os.makedirs(target_folder, exist_ok=True)
 
 # Durchlaufen aller Bilder
 image_files = glob.glob(f'{dataset_path}/*.jpg')
-for image_path in image_files:
+image_files = image_files#[4:10]
+for image_path in tqdm(image_files, desc="Processing Images"):
+
     # YOLO Modell auf Bilder anwenden
     results = process_images(image_path, model)
     data_list = save_results(results, image_path, img_folder, txt_folder)
 
     # Textdateien filtern und speichern
-    filter_text_files(data_list)
+    data_list_filter = filter_text_files(data_list)
 
     image = Image.open(image_path)
-    for data in data_list:
-        
+    for data in data_list_filter:
+
         width, height = image.size
         cls, xy1, xy2, xy3, xy4 = data
-        x_min, y_min, x_max, y_max = xy1[0]*width, xy1[1]*height, xy3[0]*width, xy3[1]*height
-        print(x_min, y_min, x_max, y_max)
+        x1, x2, x3, x4 = xy1[0]*width, xy2[0]*width, xy3[0]*width, xy4[0]*width
+        y1, y2, y3, y4 = xy1[1]*height, xy2[1]*height, xy3[1]*height, xy4[1]*height
+        buffer = 0.02
+        x_min, y_min, x_max, y_max = min(x1, x2, x3, x4)*(1-buffer), min(y1, y2, y3, y4)*(1-buffer), max(x1, x2, x3, x4)*(1+buffer), max(y1, y2, y3, y4)*(1+buffer)
         cropped_image = image.crop((x_min, y_min, x_max, y_max))
-        text = pytesseract.image_to_string(cropped_image, lang='deu')  
-        print(text)
+
+        # OCR with pytesseract
+        conf = 0.0
+        config = '--oem 3 --psm 6 -l deu'
+
+        text = pytesseract.image_to_string(cropped_image, config=config, output_type=pytesseract.Output.STRING)
+
+        # print(x_min, y_min, x_max, y_max)
+        # print(image_path)
+        # print(text)
 
 
+
+
+print('Ende')
 
 
 
