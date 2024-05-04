e0ed816438c8ff0b03583e83094f73e449fcf2c6
DexBrox <jung.valentin98@gmail.com>
2024-05-04 13:32:29 +0200

    finished Pipeline

--- Pipeline/main.py
+++ Pipeline/main.py
@@ -19,14 +19,12 @@ model = YOLO(model_path).to('cuda:1')
 # Verzeichnisse für Ergebnisse erstellen
 img_folder = os.path.join(results_path, 'images')
 txt_folder = os.path.join(results_path, 'labels')
-target_folder = os.path.join(results_path, 'labels_beschriftung')
 os.makedirs(img_folder, exist_ok=True)
 os.makedirs(txt_folder, exist_ok=True)
-os.makedirs(target_folder, exist_ok=True)
 
 # Durchlaufen aller Bilder
 image_files = glob.glob(f'{dataset_path}/*.jpg')
-image_files = image_files#[4:10]
+image_files = image_files#[:1]
 for image_path in tqdm(image_files, desc="Processing Images"):
 
     # YOLO Modell auf Bilder anwenden
@@ -38,26 +36,37 @@ for image_path in tqdm(image_files, desc="Processing Images"):
 
     image = Image.open(image_path)
     for data in data_list_filter:
-
         width, height = image.size
         cls, xy1, xy2, xy3, xy4 = data
         x1, x2, x3, x4 = xy1[0]*width, xy2[0]*width, xy3[0]*width, xy4[0]*width
         y1, y2, y3, y4 = xy1[1]*height, xy2[1]*height, xy3[1]*height, xy4[1]*height
-        buffer = 0.02
+        buffer = 0.0
         x_min, y_min, x_max, y_max = min(x1, x2, x3, x4)*(1-buffer), min(y1, y2, y3, y4)*(1-buffer), max(x1, x2, x3, x4)*(1+buffer), max(y1, y2, y3, y4)*(1+buffer)
         cropped_image = image.crop((x_min, y_min, x_max, y_max))
 
         # OCR with pytesseract
-        conf = 0.0
+        conf = 0.01
         config = '--oem 3 --psm 6 -l deu'
 
         text = pytesseract.image_to_string(cropped_image, config=config, output_type=pytesseract.Output.STRING)
+        text_lines = text.split('\n')
+        text_single_line = ' '.join(text_lines)
 
-        # print(x_min, y_min, x_max, y_max)
-        # print(image_path)
-        # print(text)
+        # Füge den extrahierten Text dem data-Eintrag hinzu
+        data.append(text_single_line)
+
+    label_file_path = os.path.join(txt_folder, os.path.splitext(os.path.basename(image_path))[0] + '.txt')
+    with open(label_file_path, 'w') as file:
+        for data in data_list:
+            if len(data) > 5:
+                file.write(f"{data[0]} {data[1][0]} {data[1][1]} {data[2][0]} {data[2][1]} {data[3][0]} {data[3][1]} {data[4][0]} {data[4][1]} {data[5]}\n")
+            else:
+                file.write(f"{data[0]} {data[1][0]} {data[1][1]} {data[2][0]} {data[2][1]} {data[3][0]} {data[3][1]} {data[4][0]} {data[4][1]}\n")
 
 print('Ende')
+
+
+
 
 
 
