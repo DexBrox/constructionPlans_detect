e15025f69f04df9059417d34361c9801681543df
DexBrox <jung.valentin98@gmail.com>
2024-05-07 14:46:09 +0200

    adjustments

--- Pipeline/main.py
+++ Pipeline/main.py
@@ -1,22 +1,21 @@
 import os
+import pytesseract
+import logging
+import glob
+
 from ultralytics import YOLO
+from PIL import Image
+from tqdm import tqdm
+
 from detect_evaluation_functions import *
 
-import pytesseract
-from PIL import Image
-from tqdm import tqdm  # Import von tqdm
-
-import logging
+# Setup für das Modell und die Verzeichnisse
 logging.getLogger('ultralytics').setLevel(logging.ERROR)
-
 dataset_path = '/workspace/datasets/Roewaplan/images/test'
 model_path = '/workspace/main_folder/models/best.pt'
-results_path = '/workspace/main_folder/results'
+results_path = '/workspace/Pipeline/results'
 
-# Model laden
 model = YOLO(model_path).to('cuda:1')
-
-# Verzeichnisse für Ergebnisse erstellen
 img_folder = os.path.join(results_path, 'images')
 txt_folder = os.path.join(results_path, 'labels')
 os.makedirs(img_folder, exist_ok=True)
@@ -24,7 +23,7 @@ os.makedirs(txt_folder, exist_ok=True)
 
 # Durchlaufen aller Bilder
 image_files = glob.glob(f'{dataset_path}/*.jpg')
-image_files = image_files#[:1]
+
 for image_path in tqdm(image_files, desc="Processing Images"):
 
     # YOLO Modell auf Bilder anwenden
@@ -34,20 +33,20 @@ for image_path in tqdm(image_files, desc="Processing Images"):
     # Textdateien filtern und speichern
     data_list_filter = filter_text_files(data_list)
 
+    # Bildverarbeitung für jeden Datenpunkt
     image = Image.open(image_path)
     for data in data_list_filter:
         width, height = image.size
         cls, xy1, xy2, xy3, xy4 = data
         x1, x2, x3, x4 = xy1[0]*width, xy2[0]*width, xy3[0]*width, xy4[0]*width
         y1, y2, y3, y4 = xy1[1]*height, xy2[1]*height, xy3[1]*height, xy4[1]*height
-        buffer = 0.0
+        buffer = 0.01
         x_min, y_min, x_max, y_max = min(x1, x2, x3, x4)*(1-buffer), min(y1, y2, y3, y4)*(1-buffer), max(x1, x2, x3, x4)*(1+buffer), max(y1, y2, y3, y4)*(1+buffer)
         cropped_image = image.crop((x_min, y_min, x_max, y_max))
 
         # OCR with pytesseract
         conf = 0.01
         config = '--oem 3 --psm 6 -l deu'
-
         text = pytesseract.image_to_string(cropped_image, config=config, output_type=pytesseract.Output.STRING)
         text_lines = text.split('\n')
         text_single_line = ' '.join(text_lines)
@@ -55,6 +54,7 @@ for image_path in tqdm(image_files, desc="Processing Images"):
         # Füge den extrahierten Text dem data-Eintrag hinzu
         data.append(text_single_line)
 
+    # Ergebnisse in Textdateien speichern
     label_file_path = os.path.join(txt_folder, os.path.splitext(os.path.basename(image_path))[0] + '.txt')
     with open(label_file_path, 'w') as file:
         for data in data_list:
@@ -64,13 +64,3 @@ for image_path in tqdm(image_files, desc="Processing Images"):
                 file.write(f"{data[0]} {data[1][0]} {data[1][1]} {data[2][0]} {data[2][1]} {data[3][0]} {data[3][1]} {data[4][0]} {data[4][1]}\n")
 
 print('Ende')
-
-
-
-
-
-
-
-
-
-
