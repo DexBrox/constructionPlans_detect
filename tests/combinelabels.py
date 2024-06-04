80b194ed29caa54fd220c75a00100ccd7bc92025
DexBrox <jung.valentin98@gmail.com>
2024-06-04 13:02:15 +0200

    update

--- tests/combinelabels.py
+++ tests/combinelabels.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 def read_labels_from_file(file_path):
     """Liest Labels aus einer Datei und gibt sie als Liste zurück."""
@@ -11,15 +12,20 @@ def write_labels_to_file(labels, output_file_path):
     with open(output_file_path, 'w') as file:
         file.writelines(labels)
 
-
 def change_class(labels):
-    print(labels)
     for i in range(len(labels)):
         label = labels[i].split()
         label[0] = '7'
         labels[i] = ' '.join(label) + '\n'
-        print(labels[i])
     return labels
+
+def remove_strings(labels):
+    """Entfernt alle Nicht-Zahlenzeichen aus den Labels."""
+    cleaned_labels = []
+    for label in labels:
+        cleaned_label = ' '.join(re.findall(r'\d+\.?\d*', label)) + '\n'
+        cleaned_labels.append(cleaned_label)
+    return cleaned_labels
 
 def combine_labels(input_folder1, input_folder2, output_folder):
     """Kombiniert Labels aus zwei Ordnern und speichert sie unter demselben Namen in einem neuen Ordner."""
@@ -50,9 +56,13 @@ def combine_labels(input_folder1, input_folder2, output_folder):
         output_file_path = os.path.join(output_folder, file_name)
         write_labels_to_file(combined_labels, output_file_path)
 
+        # Entfernen aller Strings aus den kombinierten Labels und erneut speichern
+        cleaned_labels = remove_strings(combined_labels)
+        write_labels_to_file(cleaned_labels, output_file_path)
+
 # Beispielverwendung
 input_folder1 = '/workspace/cvat2yolo-conversion-main/labels_ocr'
 input_folder2 = input_folder1.replace('labels_ocr', 'labels_od')
-output_folder = input_folder1.replace('labels_ocr', 'labels_all')
+output_folder = '/workspace/datasets/Theo/labels/train' #input_folder1.replace('labels_ocr', 'labels_all')
 
 combine_labels(input_folder1, input_folder2, output_folder)
