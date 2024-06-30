71a11e1c68a4927058e43ad2770240c976a8a94c
DexBrox <jung.valentin98@gmail.com>
2024-06-30 09:22:53 +0200

    update

--- tests/stats_boxes_obb.py
+++ tests/stats_boxes_obb.py
@@ -18,27 +18,37 @@ def is_rotated_box(coordinates, tolerance=0.00):
 
 def count_rotated_boxes(label_folders):
     rotated_boxes_percentage = {}
+    total_rotated_boxes_count = 0
+    total_boxes_count = 0
+    
     for folder in label_folders:
         rotated_boxes_count = 0
-        total_boxes_count = 0
+        folder_total_boxes_count = 0
         for label_file in glob.glob(os.path.join(folder, '*.txt')):
             with open(label_file, 'r') as file:
                 for line in file:
                     parts = line.split()
                     coordinates = list(map(float, parts[1:]))
+                    folder_total_boxes_count += 1
                     total_boxes_count += 1
                     if is_rotated_box(coordinates):
                         rotated_boxes_count += 1
-        rotated_boxes_percentage[folder] = (rotated_boxes_count / total_boxes_count) * 100 if total_boxes_count > 0 else 0
-    return rotated_boxes_percentage
+                        total_rotated_boxes_count += 1
+        rotated_boxes_percentage[folder] = (rotated_boxes_count / folder_total_boxes_count) * 100 if folder_total_boxes_count > 0 else 0
+    
+    total_percentage = (total_rotated_boxes_count / total_boxes_count) * 100 if total_boxes_count > 0 else 0
+    
+    return rotated_boxes_percentage, total_percentage
 
 label_folder1 = '/workspace/datasets/standard/Roewaplan_v3/labels/train'
 label_folder2 = label_folder1.replace('train', 'val')
 label_folder3 = label_folder1.replace('train', 'test')
 
-rotated_boxes_percentage = count_rotated_boxes([label_folder1, label_folder2, label_folder3])
+rotated_boxes_percentage, total_percentage = count_rotated_boxes([label_folder1, label_folder2, label_folder3])
 
 print(f"Prozentuale Verteilung der gedrehten Bounding Boxen:")
 for folder, percentage in rotated_boxes_percentage.items():
     folder_name = os.path.basename(folder)
     print(f"{folder_name}: {percentage:.2f}%")
+    
+print(f"Gesamtprozentuale Verteilung der gedrehten Bounding Boxen: {total_percentage:.2f}%")
